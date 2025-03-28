bbc113472a332a126107b7136a5dc7dc2928c2c7
MesVortex <meskinemostafa4@gmail.com>
2025-03-28 12:38:42 +0000

    Add last touches

--- SwiftGoals/database/migrations/2024_04_18_145140_create_images_table.php
+++ SwiftGoals/database/migrations/2024_04_18_145140_create_images_table.php
@@ -22,6 +22,9 @@ return new class extends Migration
     /**
      * Reverse the migrations.
      */
+    /**
+     * Reverse the migrations.
+     */
     public function down(): void
     {
         Schema::dropIfExists('images');
